5f7353551ca8b3331ebe36379e0e1f6393c0e0d7
Leonard Senanu <senanuld@mail.uc.edu>
2025-10-27 23:50:37 -0400

    ArrayStuff

---
+++ src/Lab_09_ArrayStuff.java
@@ -0,0 +1,37 @@
+import java.util.Random;
+
+public class Lab_09_ArrayStuff {
+    public static void main(String[] args) {
+        int[] dataPoint = new int[100];
+        Random rand = new Random();
+        for (int i = 0; i < dataPoint.length; i++) {
+            dataPoint[i] = rand.nextInt(100) + 1;
+
+        }
+
+        for (int i = 0; i < dataPoint.length; i++) {
+            System.out.print(dataPoint[i] + " ");
+            if (i < dataPoint.length - 1) {
+                System.out.print("| ");
+
+            }
+            int sum = 0;
+            for (int value : dataPoint) {
+                sum += value;
+            }
+            double average = sum / dataPoint.length;
+            System.out.print("The sum of the random array dataPoint is " + sum);
+            System.out.print(" and the average of the random array dataPoint is " + average);
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
